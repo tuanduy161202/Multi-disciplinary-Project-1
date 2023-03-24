d57d1b77628a24c3b7e192faa87c972cf8026187
Duy Hà <94681883+halac123b@users.noreply.github.com>
2023-03-24 22:15:20 +0700

    Add files via upload

---
+++ frontend/Kotlin/AddCmd.kt
@@ -0,0 +1,11 @@
+package com.halac123b.smarthome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCmd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_cmd)
+    }
+}
